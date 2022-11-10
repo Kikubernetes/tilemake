b941762f02ea24167c97351ef9dbdde654d39713
Kikubernetes <kikukikukiku2006@yahoo.co.jp>
2022-11-11 00:36:21 +0900

    add new files and modified.

--- README.md
+++ README.md
@@ -1,12 +1,65 @@
 # tilemake
-## DICOM（or NiFTI）画像を使ってPowerpoint スライドを作る！
+
+## DICOM（or NiFTI）画像を使ってPowerpoint スライドを作る
+
+![0](2022-11-10-22-58-00.png)
 
 材料：DICOMファイル（2−6シリーズ）
 
 道具：MRtrix3、FSL、tilemake.sh
-*インストール方法はこちら
 
-下ごしらえ
+* インストール方法はこちら
+
+  * MRtrix3　<https://github.com/MRtrix3/homebrew-mrtrix3>
+  * FSL <https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation>
+
+### 下ごしらえ
+
+#### mrviewのscreencaptureを使ってスライドに載せたい画像のpngファイルを作る
+
 1. DICOM画像の場合、NiFTIに変換しておく（dcm2niixなどを使う）
-1. mrviewで開く（syncview dを使うと一気に開ける）
-1. view→Toggle all annotationsで画面の文字やhair lineを消す
+1. mrviewで開く
+1. view→Toggle all annotationsで画面の文字やcrosshairを消す
+1. 必要に応じて画像の調整（intensityや向きなど）
+1. Tools→Screen captureを選ぶ
+
+![1](2022-11-10-21-17-12.png) ![2](2022-11-10-21-17-51.png) ![3](2022-11-10-21-18-24.png)
+
+1. 横断像を同期してページングする（読影ビューアっぽい見た目にする）には上図のように設定する。
+    1. 画像をスクロールして開始したい位置を表示する
+    2. Translateの右端のボックス（Z軸）を終了位置に合わせた数値にする
+    3. CaptureのStart Indexは0、Frames は欲しい枚数
+    4. OutputのPrefixで画像の名前を決める（T2、DWIなど簡単な方がgood）
+    5. 出力先のディレクトリを選ぶ
+    6. 設定したら左下の三角ボタンでテストプレイを行い調整する
+    7. 1-3の条件を揃えればスライス厚に関わらず位置を同期することができる
+1. 録音ボタン（赤丸）でpngとして保存される。
+1. １枚のスライドに載せたい画像を揃えたら下ごしらえ完了。
+
+### 調理
+
+#### tilemake.shを使ってタイル状に並んだpngファイルを作る
+
+上記を保存したディレクトリを~/imgdirとすると
+
+```bash
+cp ~/Downloads/tilemake.sh ~/imgdir　#imgdirにコピー
+cd ~/imgdir
+chmod 755 tilemake.sh　#実行可能にする
+bash tilemake.sh　#実行
+```
+
+- １画像あたり何枚のpngファイルがありますか?＞　には並べたい画像の1種類あたりの枚数を指定(上記だと22枚)
+- 並べたい順に画像の名前をスペースで区切って入力して下さい＞　には並べたい画像の名前をスペース区切りで記載
+  - ScreenCaptureで画像を保存した時のPrefix
+  - 例)T1、T2
+  - 2-6種類を選ぶ
+- タイル状に並んだpngファイルがtilesフォルダ内に出力される。
+
+### 仕上げ
+
+#### できたタイル画像をパワーポイントに１枚ずつ貼る
+
+PowerPointには複数画像を１枚ずつインポートするフォトアルバム機能があるが、Mac版にはない場合がある（地味な意地悪やめてほしい・・・）ので、LibreOfficeを使うのが楽。(こちら <https://ja.libreoffice.org> からダウンロードできる)
+インストール後、Impressを開いて挿入＞メディア＞フォトアルバムを選ぶと１枚ずつ挿入できる。その後.pptx形式で保存すればPowerPointにスライドごとコピペできる。
+![4](2022-11-10-21-13-58.png)
--- tilemake.sh
+++ tilemake.sh
@@ -1,17 +1,73 @@
 #!/bin/bash
 
-#make tiles for images.
+# Make tiles from mrview screen captured pngs. Minimum 2 series to Maximum 6 series.
+# First make screen capture your image with mrview and save as pngs (same number from all images).
+# mrviewのScreenCaptureでスライドにしたいpng画像を保存して下さい。
+# 一枚のスライドに2−6枚の画像を並べることができます。
 
+read -p "１画像あたり何枚のpngファイルがありますか?> " nump
+num=$((nump-1))
+echo "画像の枚数は${nump}枚です"
+read -p "並べたい順に画像の名前をスペースで区切って入力して下さい> " -a ary
+numary=${#ary[@]}
+echo "${ary[@]}の${numary}種類の画像からタイル画像を作ります"
+
+case $numary in
+    [0-1])  echo "2-6種類の画像を選んでください。"
+            exit ;;
+    2)
+    for i in `seq -f "%04g" 0 1 $num`; do
+	    pngappend \
+	    ${ary[0]}${i}.png + \
+        ${ary[1]}${i}.png  \
+        tile$i.png 
+    done
+     ;;
+    3)
+    for i in `seq -f "%04g" 0 1 $num`; do
+	    pngappend \
+	    ${ary[0]}${i}.png + \
+        ${ary[1]}${i}.png + \
+        ${ary[2]}${i}.png  \
+        tile$i.png 
+    done
+     ;;
+    4)
+    for i in `seq -f "%04g" 0 1 $num`; do
+	    pngappend \
+	    ${ary[0]}${i}.png + \
+        ${ary[1]}${i}.png + \
+        ${ary[2]}${i}.png - \
+        ${ary[3]}${i}.png  \
+        tile$i.png 
+    done
+     ;;
+    5)
+    for i in `seq -f "%04g" 0 1 $num`; do
+	    pngappend \
+	    ${ary[0]}${i}.png + \
+        ${ary[1]}${i}.png + \
+        ${ary[2]}${i}.png - \
+        ${ary[3]}${i}.png + \
+        ${ary[4]}${i}.png  \
+        tile$i.png 
+    done
+     ;;
+    6)  
+    for i in `seq -f "%04g" 0 1 $num`; do
+	    pngappend \
+	    ${ary[0]}${i}.png + \
+        ${ary[1]}${i}.png + \
+        ${ary[2]}${i}.png - \
+        ${ary[3]}${i}.png + \
+        ${ary[4]}${i}.png + \
+        ${ary[5]}${i}.png \
+        tile$i.png 
+    done
+     ;;
+     *) echo "2-6種類の画像を選んでください。" 
+        exit ;;
+esac
 mkdir tiles
-for i in `seq -f "%03g" 8 2 82`; do
-	k=`echo $i | sed 's/0*\([0-9]*[0-9]$\)/\1/g'`
-	j=`printf "%03d" $((k*2))`
-	convert -geometry 109x109 photo_T1_sag/T1_$j.png photo_T1_sag/sT1_$j.png
-	pngappend photo_origb0_sag/b0AP_before_topup_$i.png + \
-        photo_origb0PA_sag/b0PA_before_topup_$i.png + \
-        photo_T1_sag/sT1_$j.png - \
-        photo_after_synb0_sag/b0_u_$i.png + \
-        photo_after_synTOPUP_sag/b0AP_after_synTOPUP_$i.png + \
-        photo_conT_sag/b0AP_after_convtopup_$i.png tile$i.png 2> /dev/null
-done
 mv tile*.png tiles/
+exit
